25cbeaee57da14ea5acd367b2aa789c85450efd4
edbsjr <eduardobsjunior@hotmail.com>
2025-02-20 00:11:43 +0000

    Realiza melhorias no codigo

--- EmpregadosDesafio/src/Main.java
+++ EmpregadosDesafio/src/Main.java
@@ -9,43 +9,50 @@ public class Main {
 		String nome, sobrenome;
 		double salario;
 
-		//while (nome != "") {
-			System.out.println("Digite o nome do Empregado:");
-			nome = input.nextLine();
-			
-			System.out.println("Digite o sobrenome do Empregado:");// PRIMEIRO EMPREGADO
-			sobrenome= input.nextLine();
-			
-			System.out.println("Digite o salario anual do Empregado:");
-			salario= input.nextDouble();
-			
-			Empregado primeiroEmpregado = new Empregado(nome, sobrenome, salario);
-			
-			
-			System.out.println("Digite o nome do Empregado:");
-			nome = input.nextLine();
-			
-			System.out.println("Digite o sobrenome do Empregado:");// PRIMEIRO EMPREGADO
-			sobrenome= input.nextLine();
-			
-			System.out.println("Digite o salario anual do Empregado:");
-			salario= input.nextDouble();
-			
-			Empregado segundoEmpregado = new Empregado(nome, sobrenome, salario);
-			
-			
-			System.out.println(primeiroEmpregado.getNome()+" "+primeiroEmpregado.getSobrenome()+" recebe"
-					+ " o salario anual de R$"+primeiroEmpregado.getSalario());
-			primeiroEmpregado.aumento();
-			System.out.println(primeiroEmpregado.getNome()+" "+primeiroEmpregado.getSobrenome()+" vai receber"
-					+ " um aumento de 10% indo paraR$"+primeiroEmpregado.getSalario());
-			
-			System.out.println(segundoEmpregado.getNome()+" "+segundoEmpregado.getSobrenome()+" recebe"
-					+ " o salario anual de R$"+segundoEmpregado.getSalario());
-			segundoEmpregado.aumento();
-			System.out.println(segundoEmpregado.getNome()+" "+segundoEmpregado.getSobrenome()+" vai receber"
-					+ " um aumento de 10% indo paraR$"+segundoEmpregado.getSalario());
-		//}
+		// while (nome != "") {
+		System.out.println("Digite o nome do Empregado:");
+		nome = input.nextLine();
+
+		System.out.println("Digite o sobrenome do Empregado:");// PRIMEIRO EMPREGADO
+		sobrenome = input.nextLine();
+
+		System.out.println("Digite o salario mensal do Empregado:");
+		salario = input.nextDouble();
+
+		Empregado primeiroEmpregado = new Empregado(nome, sobrenome, salario);
+
+		input = new Scanner(System.in);
+
+		System.out.println("Digite o nome do Empregado:");
+		nome = input.nextLine();
+
+		System.out.println("Digite o sobrenome do Empregado:");// PRIMEIRO EMPREGADO
+		sobrenome = input.nextLine();
+
+		System.out.println("Digite o salario anual do Empregado:");
+		salario = input.nextDouble();
+
+		Empregado segundoEmpregado = new Empregado(nome, sobrenome, salario);
+		
+		//IMPRESSOES
+
+		System.out.println(primeiroEmpregado.getNome() + " " + primeiroEmpregado.getSobrenome() + " recebe"
+											+ " o salario anual de R$" + primeiroEmpregado.getSalario()*12);
+		System.out.println(segundoEmpregado.getNome() + " " + segundoEmpregado.getSobrenome() + " recebe"
+											+ " o salario anual de R$" + segundoEmpregado.getSalario()*12);
+		// Atualizacao do salario
+		
+		System.out.println("De quanto sera o aumento(%) do primeiro empregado:");
+		double aumento1 = input.nextDouble();
+		primeiroEmpregado.setSalario(primeiroEmpregado.getSalario()*(1+(100/aumento1)));
+		System.out.println("De quanto sera o aumento(%) do segundo empregado:");
+		double aumento2 = input.nextDouble();
+		segundoEmpregado.setSalario(segundoEmpregado.getSalario()*(1+(100/aumento2)));
+		
+		System.out.println(primeiroEmpregado.getNome() + " " + primeiroEmpregado.getSobrenome() + " recebe"
+											+ " o salario anual de R$" + primeiroEmpregado.getSalario()*12);
+		System.out.println(segundoEmpregado.getNome() + " " + segundoEmpregado.getSobrenome() + " recebe"
+											+ " o salario anual de R$" + segundoEmpregado.getSalario()*12);
 
 	}
 
